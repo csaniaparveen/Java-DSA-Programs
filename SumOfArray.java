9067c8a1c3586ac5af3c89e79b90acc67d2ccd71
csaniaparveen <saniass2022@gmail.com>
2026-01-29 13:17:26 +0530

    Create SumOfArray.java

---
+++ SumOfArray.java
@@ -0,0 +1,13 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println("Sum of array elements = " + sum);
+    }
+}
